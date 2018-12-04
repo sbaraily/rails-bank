c212caa858016961bf2276fd4771c5229cbe25dd
Samir Baraily <samir.baraily@gmail.com>
2018-12-03 20:19:56 -0700

    Done with Lecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'accounts/index'
+  get 'accounts/show'
+  get 'accounts/new'
+  get 'accounts/edit'
+  root 'accounts#index'
+  devise_for :users
+  resources :accounts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
